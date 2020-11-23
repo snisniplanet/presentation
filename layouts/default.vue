442c4a7915604d81bcde36c7163a466af43f2455
Mattia Sinisi <mattia@snisni.it>
2020-11-23 21:12:19 +0100

    Navbar + Sidebar layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,167 @@
 <template>
-  <div>
+  <div class="hidden">
+    <vs-navbar v-model="active" shadow square center-collapsed>
+      <template #left>
+        <vs-button flat icon @click="activeSidebar = !activeSidebar">
+          <i class="bx bx-menu"></i>
+        </vs-button>
+      </template>
+      <vs-navbar-item id="guide" :active="active == 'guide'">
+        Guide
+      </vs-navbar-item>
+      <vs-navbar-item id="docs" :active="active == 'docs'">
+        Documents
+      </vs-navbar-item>
+      <vs-navbar-item id="components" :active="active == 'components'">
+        Components
+      </vs-navbar-item>
+      <vs-navbar-item id="license" :active="active == 'license'">
+        license
+      </vs-navbar-item>
+      <template #right>
+        <vs-button flat>Login</vs-button>
+        <vs-button>Get Started</vs-button>
+      </template>
+    </vs-navbar>
+    <vs-sidebar v-model="active" :open.sync="activeSidebar" absolute>
+      <template #logo>
+        <!-- ...img logo -->
+      </template>
+      <vs-sidebar-item id="home">
+        <template #icon>
+          <i class="bx bx-home"></i>
+        </template>
+        Home
+      </vs-sidebar-item>
+      <vs-sidebar-item id="market">
+        <template #icon>
+          <i class="bx bx-grid-alt"></i>
+        </template>
+        Market Overview
+      </vs-sidebar-item>
+      <vs-sidebar-item id="Music">
+        <template #icon>
+          <i class="bx bxs-music"></i>
+        </template>
+        Music
+      </vs-sidebar-item>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="bx bx-group"></i>
+            </template>
+            Social media
+          </vs-sidebar-item>
+        </template>
+
+        <vs-sidebar-item id="Instagram">
+          <template #icon>
+            <i class="bx bxl-instagram"></i>
+          </template>
+          Instagram
+        </vs-sidebar-item>
+        <vs-sidebar-item id="twitter">
+          <template #icon>
+            <i class="bx bxl-twitter"></i>
+          </template>
+          Twitter
+        </vs-sidebar-item>
+        <vs-sidebar-item id="Facebook">
+          <template #icon>
+            <i class="bx bxl-facebook"></i>
+          </template>
+          Facebook
+        </vs-sidebar-item>
+      </vs-sidebar-group>
+      <vs-sidebar-group>
+        <template #header>
+          <vs-sidebar-item arrow>
+            <template #icon>
+              <i class="bx bx-code-alt"></i>
+            </template>
+            Coding
+          </vs-sidebar-item>
+        </template>
+
+        <vs-sidebar-item id="github">
+          <template #icon>
+            <i class="bx bxl-github"></i>
+          </template>
+          Github
+        </vs-sidebar-item>
+        <vs-sidebar-item id="codepen">
+          <template #icon>
+            <i class="bx bxl-codepen"></i>
+          </template>
+          Codepen
+        </vs-sidebar-item>
+        <vs-sidebar-item id="discord">
+          <template #icon>
+            <i class="bx bxl-discord"></i>
+          </template>
+          Discord
+        </vs-sidebar-item>
+        <vs-sidebar-item id="Javascript">
+          <template #icon>
+            <i class="bx bxl-javascript"></i>
+          </template>
+          Javascript
+        </vs-sidebar-item>
+        <vs-sidebar-item id="git">
+          <template #icon>
+            <i class="bx bxl-git"></i>
+          </template>
+          Git
+        </vs-sidebar-item>
+      </vs-sidebar-group>
+      <vs-sidebar-item id="donate">
+        <template #icon>
+          <i class="bx bxs-donate-heart"></i>
+        </template>
+        Donate
+      </vs-sidebar-item>
+      <vs-sidebar-item id="drink">
+        <template #icon>
+          <i class="bx bx-drink"></i>
+        </template>
+        Drink
+      </vs-sidebar-item>
+      <vs-sidebar-item id="shopping">
+        <template #icon>
+          <i class="bx bxs-shopping-bags"></i>
+        </template>
+        Shopping
+      </vs-sidebar-item>
+      <vs-sidebar-item id="chat">
+        <template #icon>
+          <i class="bx bx-chat"></i>
+        </template>
+        Chat
+      </vs-sidebar-item>
+      <template #footer>
+        <vs-row justify="space-between">
+          <vs-avatar badge-color="danger" badge-position="top-right">
+            <i class="bx bx-bell"></i>
+
+            <template #badge> 28 </template>
+          </vs-avatar>
+
+          <vs-avatar>
+            <img src="/avatars/avatar-5.png" alt="" />
+          </vs-avatar>
+        </vs-row>
+      </template>
+    </vs-sidebar>
+
     <Nuxt />
   </div>
 </template>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data: () => ({
+    active: 'home',
+    activeSidebar: false,
+  }),
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
