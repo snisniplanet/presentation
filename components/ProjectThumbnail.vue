953968bcc30ecafc9281c7e55625cd6eb5cf60d1
Mattia Sinisi <mattia@snisni.it>
2020-11-23 21:13:07 +0100

    Created base project card

---
+++ components/ProjectThumbnail.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="box">
+      <div :style="boxStyling" class="image"></div>
+    </div>
+
+    <div class="overlay">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  width: 200px;
+  height: 300px;
+  position: relative;
+}
+
+.image {
+  position: absolute;
+  inset: 0;
+
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.box,
+.image {
+  border-radius: 10px;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      default: require('~/assets/img/thumbnails/lines.jpg'),
+    },
+  },
+  data() {
+    return {
+      boxStyling: `background-image: url(${this.image})`,
+    }
+  },
+}
+</script>
